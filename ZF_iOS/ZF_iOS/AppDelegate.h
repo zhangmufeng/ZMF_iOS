c80327a91da1fa859c1212982719c9a97380bff5
张木锋 <apple@ZMF.local>
2017-09-26 23:22:38 +0800

    目录划分

---
+++ ZF_iOS/ZF_iOS/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ZF_iOS
+//
+//  Created by 张木锋 on 2017/9/26.
+//  Copyright © 2017年 张木锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
